8cb5a5da69a279f737393695c2823b42924983be
Sebastian Lenz <github@sebastian-lenz.de>
2021-01-29 12:34:14 +0100

    Use abstracted numeration in default numeration view

--- src/views/Numeration/index.ts
+++ src/views/Numeration/index.ts
@@ -1,8 +1,12 @@
 import { collectionChangedEvent } from '../CollectionView';
 import { CycleableView, transistEvent } from '../CycleableView';
-import { event, property, View, ViewOptions } from '../../core';
+import { property } from '../../core';
 import { isString } from '../../utils/lang/string';
 import { on } from '../../utils/dom/event';
+import {
+  AbstractNumeration,
+  AbstractNumerationOptions,
+} from './AbstractNumeration';
 
 export const numerationChangeEvent = 'tyny:numerationChange';
 
@@ -11,23 +15,12 @@ export interface NumerationEventArgs {
   target: Numeration;
 }
 
-export interface NumerationOptions extends ViewOptions {
-  container?: HTMLElement | string;
+export interface NumerationOptions extends AbstractNumerationOptions {
   ignoreUndecided?: boolean;
-  itemClass?: string;
-  itemTagName?: string;
-  selectedItemClass?: string;
-  target?: CycleableView | string;
 }
 
-export class Numeration extends View {
-  public container: HTMLElement;
+export class Numeration extends AbstractNumeration {
   public ignoreUndecided?: boolean;
-  public itemClass: string;
-  public items: HTMLElement[] = [];
-  public itemTagName: string;
-  public selectedItemClass: string;
-  public selectedRange: tyny.Interval = { min: -1, max: -1 };
   private _targetListeners: Function[] | null = null;
 
   @property({ immediate: true, watch: 'onTargetChanged' })
@@ -43,104 +36,10 @@ export class Numeration extends View {
   }
 
   constructor(options: NumerationOptions = {}) {
-    super({
-      tagName: 'ul',
-      ...options,
-    });
+    super(options);
 
-    const {
-      ignoreUndecided = true,
-      itemClass = `${this.component.className}--item`,
-      itemTagName = 'li',
-      selectedItemClass = 'selected',
-    } = options;
-
+    const { ignoreUndecided = true } = options;
     this.ignoreUndecided = ignoreUndecided;
-    this.itemClass = itemClass;
-    this.itemTagName = itemTagName;
-    this.selectedItemClass = selectedItemClass;
-    this.container = this.params.element({
-      name: 'container',
-      defaultValue: this.el,
-    });
-  }
-
-  setLength(length: number) {
-    const { container, itemClass, items, itemTagName } = this;
-    let itemsLength = items.length;
-
-    if (itemsLength == length) {
-      return this;
-    }
-
-    while (itemsLength < length) {
-      let item = document.createElement(itemTagName);
-      item.className = itemClass;
-      item.innerText = `${itemsLength + 1}`;
-
-      items.push(item);
-      itemsLength += 1;
-      container.appendChild(item);
-    }
-
-    while (itemsLength > length) {
-      let child = items.pop();
-      if (child) {
-        container.removeChild(child);
-      }
-
-      itemsLength -= 1;
-    }
-
-    this.setSelected(this.selectedRange);
-  }
-
-  setSelected(range: tyny.Interval) {
-    const { items, selectedRange, selectedItemClass } = this;
-    const length = items.length - 1;
-    let { min, max } = range;
-    min = Math.max(-1, Math.min(length, Math.floor(min)));
-    max = Math.max(-1, Math.min(length, Math.ceil(max)));
-
-    if (max < min) {
-      let tmp = min;
-      min = max;
-      max = tmp;
-    }
-
-    if (selectedRange.min == min && selectedRange.max == max) {
-      return this;
-    }
-
-    for (let index = 0; index <= length; index++) {
-      items[index].classList.toggle(
-        selectedItemClass,
-        index >= min && index <= max
-      );
-    }
-
-    selectedRange.min = min;
-    selectedRange.max = max;
-  }
-
-  setSelectedIndex(index: number) {
-    this.setSelected({ min: index, max: index });
-  }
-
-  @event({ name: 'click' })
-  protected onClick(event: MouseEvent) {
-    const { el, items } = this;
-    let target = event.target as HTMLElement | null;
-
-    while (target) {
-      const index = items.indexOf(target);
-      if (index !== -1) {
-        return this.selectIndex(index);
-      }
-
-      if (target === el) return;
-      target = target.parentElement;
-    }
   }
 
   protected onCurrentChanged() {
@@ -167,7 +66,7 @@ export class Numeration extends View {
   protected onTargetChanged(target: CycleableView | null) {
     const { _targetListeners } = this;
     if (_targetListeners) {
-      _targetListeners.forEach((listener) => listener());
+      _targetListeners.forEach((off) => off());
     }
 
     if (target) {

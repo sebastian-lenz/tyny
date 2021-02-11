909158b59939b24436d3a28e71c4a32d9da2d5b4
Sebastian Lenz <github@sebastian-lenz.de>
2021-02-11 18:18:00 +0100

    Add hasScrollbars helper

--- lib/services/scrollbars.d.ts
+++ lib/services/scrollbars.d.ts
@@ -1,2 +1,3 @@
 export declare function getScrollBarSize(): number;
+export declare function hasScrollbars(): boolean;
 export declare function toggleScrollbars(origin: any, enabled: boolean): void;

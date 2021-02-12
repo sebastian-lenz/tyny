0cd423ad3a5a2c54e15bbc4152c37d0ef3ee3dfe
Sebastian Lenz <github@sebastian-lenz.de>
2021-02-12 17:24:17 +0100

    Add scrollbars service change event

--- lib/services/scrollbars.d.ts
+++ lib/services/scrollbars.d.ts
@@ -1,3 +1,8 @@
+export declare const scrollbarsChangeEvent = "tyny:scrollbarsChange";
+export interface ScrollbarsEventArgs {
+    hasScrollbars: boolean;
+    scrollBarSize: number;
+}
 export declare function getScrollBarSize(): number;
 export declare function hasScrollbars(): boolean;
 export declare function toggleScrollbars(origin: any, enabled: boolean): void;

5e8ccbbeb2f259025cd15b4f5e0ad0758148b764
Sebastian Lenz <github@sebastian-lenz.de>
2021-03-03 23:30:04 +0100

    Add parentsAndSelf helper

--- src/utils/dom/node/parents.ts
+++ src/utils/dom/node/parents.ts
@@ -3,16 +3,26 @@ import { toElement } from '../misc/toElement';
 
 export function parents(
   element: tyny.ElementLike,
-  selector?: string
+  selector?: string,
+  self?: boolean
 ): HTMLElement[] {
   const parents: HTMLElement[] = [];
   let parent = toElement(element);
+  if (!self && parent) {
+    parent = parent.parentElement;
+  }
 
-  while (parent && (parent = parent.parentElement)) {
+  while (parent) {
     if (!selector || matches(parent, selector)) {
       parents.push(parent);
     }
+
+    parent = parent.parentElement;
   }
 
   return parents;
+}
+
+export function parentsAndSelf(element: tyny.ElementLike, selector?: string) {
+  return parents(element, selector, true);
 }

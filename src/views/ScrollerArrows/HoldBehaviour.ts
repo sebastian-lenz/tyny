dedda6f2277ef604622d0242257161a32aa5a6fc
Sebastian Lenz <github@sebastian-lenz.de>
2021-03-03 23:31:01 +0100

    Add hold behaviour and scroller arrows

---
+++ src/views/ScrollerArrows/HoldBehaviour.ts
@@ -0,0 +1,85 @@
+import { addFrameHook, removeFrameHook } from '../../fx/dispatcher';
+import { NativeEvent } from '../../core/pointers/PointerBehaviour';
+import { Pointer } from '../../core/pointers/Pointer';
+import { parentsAndSelf } from '../../utils/dom/node';
+import { ScrollerArrows } from './index';
+import { Spring, spring } from '../../fx/spring';
+import {
+  HoldBehaviour as HoldBehaviourBase,
+  HoldStage,
+} from '../../core/pointers/HoldBehaviour';
+
+export class HoldBehaviour extends HoldBehaviourBase<ScrollerArrows> {
+  animation: Spring | null = null;
+  forward: number = 0;
+  multiplier: number = 0;
+  position: tyny.Point = { x: 0, y: 0 };
+  speed: number = 10;
+
+  constructor(view: ScrollerArrows) {
+    super(view, {
+      stages: [{ delay: 250 }, { delay: 1000 }, { delay: 1000 }],
+    });
+  }
+
+  onBeginHold(event: NativeEvent, _pointer: Pointer): boolean {
+    const { backward, forward } = this.view;
+    this.forward = parentsAndSelf(event.target).reduce((result, parent) => {
+      if (parent === backward) return -1;
+      if (parent === forward) return 1;
+      return result;
+    }, 0);
+
+    return this.forward !== 0;
+  }
+
+  onEndHold() {
+    this.animation = null;
+    removeFrameHook(this.onFrame);
+  }
+
+  onFrame = () => {
+    const { animation, forward, multiplier, speed, view } = this;
+    const { direction, target } = view;
+    let { position } = this;
+    if (!target) {
+      return;
+    }
+
+    const axis = direction === 'horizontal' ? 'x' : 'y';
+    position[axis] += forward * multiplier * speed;
+    position = this.position = target.clampPosition(position);
+
+    if (animation) {
+      animation.advance({ position });
+    } else {
+      this.animation = spring(target, { position });
+    }
+  };
+
+  onStageAbort(_stage: HoldStage, index: number) {
+    const { forward } = this;
+    const { direction, target } = this.view;
+
+    if (index === 0 && target) {
+      const { position } = target;
+      const axis = direction === 'horizontal' ? 'x' : 'y';
+      const dimension = direction === 'horizontal' ? 'width' : 'height';
+      const size = target.viewportSize[dimension];
+
+      position[axis] = Math.round(position[axis] / size + forward) * size;
+      target.tweenTo(target.clampPosition(position));
+    }
+  }
+
+  onStageEnter(_stage: HoldStage, index: number) {
+    if (index === 0) {
+      const { target } = this.view;
+      this.position = target ? target.position : { x: 0, y: 0 };
+      this.multiplier = 1;
+      addFrameHook(this.onFrame);
+    } else {
+      this.multiplier *= 2.5;
+    }
+  }
+}
---
+++ src/views/ScrollerArrows/index.ts
@@ -0,0 +1,68 @@
+import { isString } from '../../utils/lang/string';
+import { HoldBehaviour } from './HoldBehaviour';
+import { on } from '../../utils/dom/event';
+import { property, View, ViewOptions } from '../../core';
+import { Scroller, scrollerScrollEvent } from '../Scroller';
+import { DragDirection } from '../../core/pointers/DragBehaviour';
+
+const buttonParam = (name: string) => ({
+  className: `${process.env.TYNY_PREFIX}ScrollerArrows--button ${name}`,
+  defaultValue: `button.${name}`,
+  tagName: 'button',
+  type: 'element' as 'element',
+});
+
+export interface ScrollerArrowsOptions extends ViewOptions {
+  backward?: string | HTMLElement;
+  direction?: DragDirection;
+  forward?: string | HTMLElement;
+  target?: Scroller | string;
+}
+
+export class ScrollerArrows extends View {
+  protected _targetListeners: Function[] | null = null;
+
+  @property({ param: { defaultValue: 'horizontal', type: 'string' } })
+  direction!: DragDirection;
+
+  @property({ immediate: true, param: buttonParam('backward') })
+  backward!: HTMLButtonElement;
+
+  @property({ immediate: true, param: buttonParam('forward') })
+  forward!: HTMLButtonElement;
+
+  constructor(options: ViewOptions) {
+    super(options);
+
+    this.addBehaviour(HoldBehaviour);
+  }
+
+  @property({ immediate: true, watch: 'onTargetChanged' })
+  get target(): Scroller | null {
+    const target = this.params.read<any>({ name: 'target' });
+    if (target instanceof Scroller) {
+      return target;
+    } else if (isString(target)) {
+      return this.findView(target, Scroller);
+    } else {
+      return null;
+    }
+  }
+
+  protected onScrollerChanged() {}
+
+  protected onTargetChanged(target: Scroller | null) {
+    const { _targetListeners } = this;
+    if (_targetListeners) {
+      _targetListeners.forEach((off) => off());
+    }
+
+    if (target) {
+      this._targetListeners = [
+        on(target.el, scrollerScrollEvent, this.onScrollerChanged, {
+          scope: this,
+        }),
+      ];
+    }
+  }
+}

9e59dce36f1a9a37bec49e727e11c6878ec41999
Sebastian Lenz <github@sebastian-lenz.de>
2022-07-06 10:17:01 +0200

    Make component name mandatory

--- src/core/decorators/component.ts
+++ src/core/decorators/component.ts
@@ -1,11 +1,9 @@
 import { registerView } from '../components';
 
-export function component(
-  options: {
-    className?: string;
-    name?: string;
-  } = {}
-): ClassDecorator {
+export function component(options: {
+  className?: string;
+  name: string;
+}): ClassDecorator {
   return function (ctor: Function) {
     registerView(options.name || ctor.name, ctor as any, options);
   };

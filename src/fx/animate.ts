4f24fc4383c93e1c9e3e70b465a675a9d466ff82
Sebastian Lenz <github@sebastian-lenz.de>
2022-08-11 17:33:12 +0200

    Fix animations not playing till the end

--- src/fx/animate.ts
+++ src/fx/animate.ts
@@ -1,4 +1,8 @@
-import { animation, onAnimationEnd } from '../utils/env/animationProps';
+import {
+  animation,
+  onAnimationEnd,
+  onAnimationStart,
+} from '../utils/env/animationProps';
 
 export type FillMode = 'none' | 'forwards' | 'backwards' | 'both';
 export type Direction =
@@ -33,17 +37,33 @@ function animate(
   return new Promise((resolve) => {
     const style = element.style as any;
     const value = `${name} ${duration}ms ${timingFunction} ${delay}ms ${direction} ${fillMode}`;
+    let timeout: number = 0;
 
-    const handleAnimationEnd = (event: Event | undefined) => {
+    function handleAnimationEnd(event: Event | undefined) {
       if (event && event.target !== element) return;
       element.removeEventListener(onAnimationEnd, handleAnimationEnd);
-      clearTimeout(timeout);
+      element.removeEventListener(onAnimationStart, handleAnimationStart);
       style[animation] = '';
+      clearTimeout(timeout);
       resolve();
-    };
+    }
 
-    const timeout = setTimeout(handleAnimationEnd, duration + delay + 100);
+    function handleAnimationStart(event: Event) {
+      if (event.target !== element) return;
+      startTimeout(100);
+    }
+
+    function startTimeout(extraTime: number) {
+      if (timeout) window.clearTimeout(timeout);
+      timeout = window.setTimeout(
+        handleAnimationEnd,
+        duration + delay + extraTime
+      );
+    }
+
+    startTimeout(500);
     element.addEventListener(onAnimationEnd, handleAnimationEnd);
+    element.addEventListener(onAnimationStart, handleAnimationStart);
     style[animation] = value;
   });
 }

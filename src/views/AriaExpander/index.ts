7f480ae482be8dc231125ec9f5a19642bda94a81
Sebastian Lenz <github@sebastian-lenz.de>
2024-10-08 12:37:38 +0200

    Add expander component

---
+++ src/views/AriaExpander/index.ts
@@ -0,0 +1,43 @@
+import { boolify } from '../../utils/lang/string';
+import { data } from '../../utils/dom/attr';
+import { event, View } from '../../core';
+import { toBoolean } from '../../utils/lang/misc';
+import { transistHeight } from '../../fx/transistHeight';
+import type { ViewOptions } from '../../core';
+
+export class AriaExpander extends View {
+  button: HTMLElement | null;
+  isExpanded: boolean;
+  target: HTMLElement | null;
+
+  constructor(options: ViewOptions) {
+    super(options);
+
+    const button = this.find('*[aria-controls]');
+    const targetId = data(button, 'aria-controls');
+
+    this.button = button;
+    this.target = targetId ? document.getElementById(targetId) : null;
+    this.isExpanded = toBoolean(data(button, 'aria-expanded'));
+  }
+
+  @event({ name: 'click', selector: '*[aria-controls]' })
+  onButtonClick(event: tyny.DelegateEvent) {
+    this.setExpanded();
+  }
+
+  setExpanded(value: boolean = !this.isExpanded) {
+    if (this.isExpanded === value) return;
+    this.isExpanded = value;
+
+    const { button, target } = this;
+    if (button) {
+      button.setAttribute('aria-expanded', boolify(value));
+    }
+
+    if (target) {
+      target.setAttribute('aria-hidden', boolify(!value));
+      transistHeight(target, () => target.classList.toggle('expanded', value));
+    }
+  }
+}

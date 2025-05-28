eded5640de18486f258f6a87a6a79d6dac3ad7ed
Sebastian Lenz <github@sebastian-lenz.de>
2025-05-28 18:24:52 +0200

    Add aria combobox

---
+++ lib/views/AriaComboBox/index.d.ts
@@ -0,0 +1,6 @@
+import type { TransistHeightOptions } from '../../fx/transistHeight';
+import type { ViewOptions } from '../../index';
+export interface AriaComboBoxOptions extends ViewOptions {
+    mask?: HTMLElement | string;
+    transition?: TransistHeightOptions;
+}
